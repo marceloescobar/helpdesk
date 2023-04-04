472b31ce4a0b379f5ef64fd1223b738ca0376f74
marceloescobar <escobar.marcelo@gmail.com>
2023-04-03 21:40:39 -0300

    add findbyid para chamados

---
+++ src/main/java/com/mescobar/helpdesk/service/ChamadoService.java
@@ -0,0 +1,23 @@
+package com.mescobar.helpdesk.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mescobar.helpdesk.domain.Chamado;
+import com.mescobar.helpdesk.exception.ObjectNotFoundException;
+import com.mescobar.helpdesk.repository.ChamadoRepository;
+
+@Service
+public class ChamadoService {
+
+	@Autowired
+	private ChamadoRepository repository;
+
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado com o ID:" + id));
+	}
+}
